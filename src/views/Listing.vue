55959fba124dfc133dd6fd372e77f6ce8330bf86
TristanLemire <tristan.lemire.sio@gmail.com>
2020-05-18 16:45:13 +0200

    WIP linsting acticles

--- src/views/Listing.vue
+++ src/views/Listing.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="about">
     <h1>This is an listing page</h1>
-    <router-link to="/article/Kurobozu">Kurobozu</router-link>
+    <ul>
+      <li><router-link to="/article/Kurobozu">Kurobozu</router-link></li>
+      <li><router-link to="/article/Oni">Oni</router-link></li>
+      <li><router-link to="/article/Tengu">Tengu</router-link></li>
+      <li><router-link to="/article/Kitsune">Kitsune</router-link></li>
+      <li><router-link to="/article/Hone_onna">Hone onna</router-link></li>
+      <li><router-link to="/article/Wanyudo">Wanyudo</router-link></li>
+      <li><router-link to="/article/Tenome">Tenome</router-link></li>
+      <li><router-link to="/article/Godzilla">Godzilla</router-link></li>
+    </ul>
   </div>
 </template>
