45255794c28b02e06eb48e1c8a5e9cad9eb13357
TristanLemire <tristan.lemire.sio@gmail.com>
2020-05-19 11:43:41 +0200

    home article

---
+++ src/components/ArticleComponent.vue
@@ -0,0 +1,27 @@
+<template>
+     <div>
+        <img :src="require(`../assets/images/${article.title}.jpeg`)">
+        <h3>{{article.title}}</h3>
+        <p>{{article.paragraphe}}</p>
+        <a>{{buttonText}}</a>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+    article: Object,
+    buttonText: String,
+    },
+}
+</script>
+
+<style scoped lang="scss">
+div {
+    width: 25%;
+
+    img{
+        width: 100%;
+    }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,58 @@
 <template>
   <div class="home">
     <HomePrez title="Qu’est ce qu’un Yokai ?" textBackground="モンスター" paragraphe="Monstre, créature, esprit, fantôme, objet, démon où tout autre apparition étrange souvent malfaisant ou simplement malicieux du folklore japonais qui incarne les obsessions, les vices, les craintes, les mal-être, les blessures, les rancoeurs et tout état émotionnel compliqué des humains. Ils représentent également des phénomènes naturels, des objets créés par les hommes ou des éléments naturels inanimés (rocher…)."/>
+    <div class="articles_section">
+      <h2>Les nouveaux articles</h2>
+      <div class="articles" >
+        <ArticleComponent v-for="article in articles" :key="article.id" :article="article" buttonText="Lire l'article"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HomePrez from '@/components/HomePrez.vue'
+import ArticleComponent from '@/components/ArticleComponent.vue'
 
 export default {
   name: 'Home',
   components: {
-    HomePrez
+    HomePrez,
+    ArticleComponent
   },
   data () {
     return {
-      title: 'hello'
+      articles: [
+        {
+          title: "Tengu",
+          paragraphe: "Demon samourai",
+        },
+        {
+          title: "Kurobozu",
+          paragraphe: "Monstre du sommeil",
+        },
+        {
+          title: "Wanyudo",
+          paragraphe: "Roue de feu",
+        },
+      ]
     }
   }
 }
 </script>
+
+<style scoped lang="scss">
+  
+  .articles_section {
+    width: 100%;
+    margin: 40px;
+
+    .articles {
+      display: flex;
+      justify-content: center;
+    }
+  }
+
+
+</style>
