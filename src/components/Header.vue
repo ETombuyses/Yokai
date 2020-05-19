cd67b0c9605e7099275822c61ed3416d5d126da5
ETombuyses <e.tombuyses@gmail.com>
2020-05-19 11:42:04 +0200

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="header" :style="{width: width}">
+      <div class="title">
+        <h1>{{ title }}</h1>
+        <span>{{ textBackground }}</span>
+      </div>
+      <p>{{paragraphe}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    textBackground: String,
+    paragraphe: String,
+    width: {
+      type: String,
+      default: '100%'
+    }
+  },
+}
+</script>
+
+<style scoped lang="scss">
+
+  * {
+    box-sizing: border-box;
+  }
+
+  .header {
+    text-align: justify;
+
+    .title {
+      position: relative;
+    }
+
+    span {
+      display: none;
+      font-size: 4em;
+      color: rgba(217, 4, 39, 0.26);
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      left: -20px;
+      opacity: 0.5;
+
+      @media screen and (min-width: 700px) {
+        display: block;
+      }
+
+      @media screen and (min-width: 950px) {
+        display: block;
+        font-size: 6em;
+      }
+    }
+  }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="home">
-    <HomePrez title="Qu’est ce qu’un Yokai ?" textBackground="モンスター" paragraphe="Monstre, créature, esprit, fantôme, objet, démon où tout autre apparition étrange souvent malfaisant ou simplement malicieux du folklore japonais qui incarne les obsessions, les vices, les craintes, les mal-être, les blessures, les rancoeurs et tout état émotionnel compliqué des humains. Ils représentent également des phénomènes naturels, des objets créés par les hommes ou des éléments naturels inanimés (rocher…)."/>
+    <img src="../assets/images/home.jpg">
+    <Header width="60%" title="Qu’est ce qu’un Yokai ?" textBackground="日本の妖怪" paragraphe="Monstre, créature, esprit, fantôme, objet, démon où tout autre apparition étrange souvent malfaisant ou simplement malicieux du folklore japonais qui incarne les obsessions, les vices, les craintes, les mal-être, les blessures, les rancoeurs et tout état émotionnel compliqué des humains. Ils représentent également des phénomènes naturels, des objets créés par les hommes ou des éléments naturels inanimés (rocher…)."/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HomePrez from '@/components/HomePrez.vue'
+import Header from '@/components/Header.vue'
 
 export default {
   name: 'Home',
   components: {
-    HomePrez
+    Header
   },
   data () {
     return {
@@ -20,3 +21,25 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+
+  * {
+    box-sizing: border-box;
+  }
+
+  .home {
+    width: 100%;
+    padding-left: 40px;
+    padding-right: 80px;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    display: flex;
+  }
+
+  img {
+    width: 40%;
+    margin-right: 30px;
+    object-fit: cover;
+  }
+</style>
