3025f2cb91dc60bdae996d40dc645036f2410ce1
TristanLemire <tristan.lemire.sio@gmail.com>
2020-05-19 09:08:22 +0200

    WIP Nav

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/listing">Listing</router-link>
+      <img class="logo" src="./assets/images/logo.png">
+      <div>
+        <router-link class="" to="/">Home</router-link>
+        <router-link to="/listing">Listing</router-link>
+      </div>
     </div>
     <router-view/>
   </div>
@@ -13,20 +16,31 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+  margin: 20px;
+  /* padding-right: 50px; */
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.logo {
+  height: 75px;
 }
 
 #nav a {
+  text-decoration: none;
+  margin-right: 10px;
   font-weight: bold;
-  color: #2c3e50;
+  color: #181818;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  font-weight: bold;
+  color: #d60000;
 }
 </style>
