c8513624cd986bf0ee294aa50a2ef071d29ff2e4
ETombuyses <e.tombuyses@gmail.com>
2020-05-19 09:09:05 +0200

    test

--- src/views/Listing.vue
+++ src/views/Listing.vue
@@ -2,14 +2,22 @@
   <div class="about">
     <h1>This is an listing page</h1>
     <ul>
-      <li><router-link to="/article/Kurobozu">Kurobozu</router-link></li>
+      <li></li>
+      <!-- <li><router-link to="/article/Kurobozu">Kurobozu</router-link></li>
       <li><router-link to="/article/Oni">Oni</router-link></li>
       <li><router-link to="/article/Tengu">Tengu</router-link></li>
       <li><router-link to="/article/Kitsune">Kitsune</router-link></li>
       <li><router-link to="/article/Hone_onna">Hone onna</router-link></li>
       <li><router-link to="/article/Wanyudo">Wanyudo</router-link></li>
       <li><router-link to="/article/Tenome">Tenome</router-link></li>
-      <li><router-link to="/article/Godzilla">Godzilla</router-link></li>
+      <li><router-link to="/article/Godzilla">Godzilla</router-link></li> -->
     </ul>
   </div>
 </template>
+<script>
+export default {
+  created () {
+    
+  }
+}
+</script>
