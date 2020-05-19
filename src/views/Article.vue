cbbd985f977ba50ba28bfa074c4eaf2faea974db
ETombuyses <e.tombuyses@gmail.com>
2020-05-19 09:29:34 +0200

    article

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,15 +1,26 @@
 <template>
-  <div>
-    Article 
-    <img :src="require(`../assets/images/${this.$route.params.slug}.jpeg`)">
+  <div v-if="post">
+      <h1>{{post.title}}</h1>
+      <p>{{post.body}}</p>
   </div>
 </template>
 
 <script>
 export default {
-  components: {
+  data () {
+    return {
+      post: null
+    }
   },
   computed: {
+  },
+  created () {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${this.$route.params.slug}`).then(response => {
+      // json() pour transformer les data en json
+      response.json().then(data => {
+        this.post = data
+      })
+    })
   }
 }
 </script>
